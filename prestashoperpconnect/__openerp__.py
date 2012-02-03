577d699bdbec26cfb9d4943013cfb9167b8ede42
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-02-03 15:13:50 +0100

    [ADD] First commit of this project :) More to come soon...

---
+++ prestashoperpconnect/__openerp__.py
@@ -0,0 +1,45 @@
+#########################################################################
+#                                                                       #
+# Copyright (C) 2012 Camptocamp                                         #
+# Copyright (C) 2012 Akretion                                           #
+#                                                                       #
+#This program is free software: you can redistribute it and/or modify   #
+#it under the terms of the GNU General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or      #
+#(at your option) any later version.                                    #
+#                                                                       #
+#This program is distributed in the hope that it will be useful,        #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#GNU General Public License for more details.                           #
+#                                                                       #
+#You should have received a copy of the GNU General Public License      #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
+{
+    "name" : "Prestashop e-commerce",
+    "version" : "1.0",
+    "depends" : ["base",
+                 "product",
+                 "product_m2mcategories",
+                 'delivery',
+                 "base_sale_multichannels",
+                 "product_images_olbs",
+                ],
+    "author" : "PrestashopERPconnect Core Editors",
+    "description": """Prestashop E-commerce management
+""",
+    'images': [
+    ],
+    "website" : "https://launchpad.net/prestashoperpconnect",
+    "category" : "Generic Modules",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [
+                    ],
+    "active": False,
+    "installable": True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
