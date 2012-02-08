324f8b38de98f93808966801aa6165d9d4919a3a
sebastien beau <sebastien.beau@akretion.com.br>
2012-02-08 20:35:56 +0100

    [IMP] add mapping template

--- prestashoperpconnect/__openerp__.py
+++ prestashoperpconnect/__openerp__.py
@@ -45,6 +45,7 @@
         'external_referential_view.xml',
         'settings/external.referential.type.csv',
         'settings/1.5.0.0/external.referential.version.csv',
+        'settings/1.5.0.0/external.mapping.template.csv',
         'settings/1.5.0.0/res.partner.address/external.mappinglines.template.csv',
         'settings/1.5.0.0/res.partner/external.mappinglines.template.csv',
         'settings/1.5.0.0/product.product/external.mappinglines.template.csv',
