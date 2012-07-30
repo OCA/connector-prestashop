44613878b939c985073fd4f56879160eb3a0eb0b
sebastien beau <sebastien.beau@akretion.com.br>
2012-07-30 18:54:52 +0200

    [FIX] remove useless mapping for 1.4 for the object sale_state and sale_history deleted before

--- prestashoperpconnect/__openerp__.py
+++ prestashoperpconnect/__openerp__.py
@@ -91,8 +91,6 @@ This connector is built on a very solid basis, but still requires deep knowledge
         'settings/1.4.0.0/sale.shop/external.mappinglines.template.csv',
         'settings/1.4.0.0/sale.order.line/external.mappinglines.template.csv',
         'settings/1.4.0.0/delivery.carrier/external.mappinglines.template.csv',
-        'settings/1.4.0.0/sale.order.state/external.mappinglines.template.csv',
-        'settings/1.4.0.0/sale.order.history/external.mappinglines.template.csv',
     ],
     "active": False,
     "installable": True,
