e90cfbe06f183219b2cd959985da2ed394bca9c4
Benoit Guillot <benoit.guillot@akretion.com.br>
2012-09-21 15:54:22 +0200

    [ADD] add module to remove sale order lines in prestashop from openerp

---
+++ prestashoperpconnect_sale_order_editor/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#   prestashoperpconnect_sale_order_editor for OpenERP                        #
+#   Copyright (C) 2012 Akretion Benoît GUILLOT <benoit.guillot@akretion.com>  #
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU Affero General Public License as            #
+#   published by the Free Software Foundation, either version 3 of the        #
+#   License, or (at your option) any later version.                           #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU Affero General Public License for more details.                       #
+#                                                                             #
+#   You should have received a copy of the GNU Affero General Public License  #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+
+
+{
+    'name': 'prestashoperpconnect_sale_order_editor',
+    'version': '0.1',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'description': """empty""",
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com/',
+    'depends': ['prestashoperpconnect'], 
+    'init_xml': [],
+    'update_xml': [ 
+           'sale_view.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
