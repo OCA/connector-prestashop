5f4d95c4005e3e62658c8aa62c47161672946407
Arthur Vuillard <arthur@hashbang.fr>
2014-06-24 17:31:50 +0200

    Fix updating the quantity in prestashop

--- prestashoperpconnect/models/stock.py
+++ prestashoperpconnect/models/stock.py
@@ -1,37 +1,6 @@
 from openerp.osv.orm import Model
 
 
-class StockPicking(Model):
-    _inherit = 'stock.picking'
-
-    def action_done(self, cr, uid, ids, context=None):
-        res = super(StockPicking, self).action_done(
-            cr, uid, ids, context=context
-        )
-        for picking in self.browse(cr, uid, ids, context=context):
-            if picking.type == 'in':
-                self.update_prestashop_quantities(
-                    cr, uid, ids, context=context
-                )
-        return res
-
-    def update_prestashop_quantities(self, cr, uid, ids, context=None):
-        for picking in self.browse(cr, uid, ids, context=context):
-            for move in picking.move_lines:
-                move.update_prestashop_quantities()
-
-    def action_cancel(self, cr, uid, ids, context=None):
-        res = super(StockPicking, self).action_cancel(
-            cr, uid, ids, context=context
-        )
-        for picking in self.browse(cr, uid, ids, context=context):
-            if picking.type == 'out':
-                self.update_prestashop_quantities(
-                    cr, uid, ids, context=context
-                )
-        return res
-
-
 class StockMove(Model):
     _inherit = 'stock.move'
 
@@ -39,21 +8,46 @@ class StockMove(Model):
         for move in self.browse(cr, uid, ids, context=context):
             move.product_id.update_prestashop_quantities()
 
-
-class StockPickingOut(Model):
-    _inherit = 'stock.picking.out'
+    def get_stock_location_ids(self, cr, uid, context=None):
+        warehouse_obj = self.pool['stock.warehouse']
+        warehouse_ids = warehouse_obj.search(cr, uid, [], context=context)
+        warehouses = warehouse_obj.browse(
+            cr, uid, warehouse_ids, context=context
+        )
+        location_ids = []
+        for warehouse in warehouses:
+            location_ids.append(warehouse.lot_stock_id.id)
+        return location_ids
 
     def create(self, cr, uid, vals, context=None):
-        picking_out_id = super(StockPickingOut, self).create(
+        stock_id = super(StockMove, self).create(
             cr, uid, vals, context=context
         )
-        self.update_prestashop_quantities(
-            cr, uid, [picking_out_id], context=context
-        )
-        return picking_out_id
+        location_ids = self.get_stock_location_ids(cr, uid, context=context)
+        if vals['location_id'] in location_ids:
+            self.update_prestashop_quantities(
+                cr, uid, [stock_id], context=context
+            )
+        return stock_id
 
-    def update_prestashop_quantities(self, cr, uid, ids, context=None):
-        picking_obj = self.pool.get('stock.picking')
-        return picking_obj.update_prestashop_quantities(
+    def action_cancel(self, cr, uid, ids, context=None):
+        res = super(StockMove, self).action_cancel(
             cr, uid, ids, context=context
         )
+        location_ids = self.get_stock_location_ids(cr, uid, context=context)
+        for move in self.browse(cr, uid, ids, context=context):
+            if move.location_id.id in location_ids:
+                self.update_prestashop_quantities(
+                    cr, uid, [move.id], context=context
+                )
+        return res
+
+    def action_done(self, cr, uid, ids, context=None):
+        res = super(StockMove, self).action_done(cr, uid, ids, context=context)
+        location_ids = self.get_stock_location_ids(cr, uid, context=context)
+        for move in self.browse(cr, uid, ids, context=context):
+            if move.location_dest_id.id in location_ids:
+                self.update_prestashop_quantities(
+                    cr, uid, [move.id], context=context
+                )
+        return res
