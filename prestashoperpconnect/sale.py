cfef5853ab57a1f060f98d0b1f59773baa2c6120
Parthiv Patel <parthivgls@gmail.com>
2015-09-26 05:25:45 -0700

    [IMP] sale order line and order status update

--- prestashoperpconnect/sale.py
+++ prestashoperpconnect/sale.py
@@ -2,7 +2,9 @@
 ###############################################################################
 #                                                                             #
 #   Prestashoperpconnect for OpenERP                                          #
-#   Copyright (C) 2013 Akretion                                               #
+#   Copyright (C) 2013 Akretion
+#   Copyright (C) 2015 Tech-Receptives(<http://www.tech-receptives.com>)
+#   @author Parthiv Patel <parthiv@techreceptives.com>                                          #
 #                                                                             #
 #   This program is free software: you can redistribute it and/or modify      #
 #   it under the terms of the GNU Affero General Public License as            #
@@ -18,19 +20,29 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
 #                                                                             #
 ###############################################################################
+
 from prestapyt import PrestaShopWebServiceDict
-
-from openerp.osv import fields, orm
-import openerp.addons.decimal_precision as dp
-from openerp.addons.connector.queue.job import job
+from backend import prestashop
 from openerp.addons.connector.event import on_record_write
+from openerp.addons.connector.queue.job import job
 from openerp.addons.connector.unit.synchronizer import (ExportSynchronizer)
-from .unit.backend_adapter import GenericAdapter
-
 from openerp.addons.connector_ecommerce.unit.sale_order_onchange import (
     SaleOrderOnChange)
 from .connector import get_environment
-from backend import prestashop
+from .unit.backend_adapter import GenericAdapter
+
+
+ORDER_STATUS_MAPPING = {
+    'draft': "Preparation in progress",
+    'sent': "Preparation in progress",
+    'manual': "Awaiting bank wire payment",
+    'progress': "Payment accepted",
+    'shipping_except': "",
+    'invoice_except': "Payment error",
+    'done': "Delivered",
+    'cancel': "Canceled",
+    'waiting_date': ""
+}
 
 
 @prestashop
@@ -71,19 +83,20 @@ class SaleOrderAdapter(GenericAdapter):
             result += api.search(self._prestashop_model, filters)
         return result
 
+
 @prestashop
 class OrderCarriers(GenericAdapter):
     _model_name = '__not_exit_prestashop.order_carrier'
     _prestashop_model = 'order_carriers'
     _export_node_name = 'order_carrier'
- 
+
 
 @prestashop
 class PaymentMethodAdapter(GenericAdapter):
     _model_name = 'payment.method'
     _prestashop_model = 'orders'
     _export_node_name = 'order'
-    
+
     def search(self, filters=None):
         api = self.connect()
         res = api.get(self._prestashop_model, options=filters)
@@ -91,6 +104,7 @@ class PaymentMethodAdapter(GenericAdapter):
         if isinstance(methods, dict):
             return [methods]
         return methods
+
 
 @prestashop
 class SaleOrderLineAdapter(GenericAdapter):
@@ -121,25 +135,11 @@ def prestashop_sale_state_modified(session, model_name, record_id,
     if 'state' in fields:
         sale = session.browse(model_name, record_id)
         # a quick test to see if it is worth trying to export sale state
-        states = session.search(
-            'sale.order.state.list',
-            [('name', '=', sale.state)]
-        )
+        new_state = ORDER_STATUS_MAPPING[sale.state]
+        states = session.search('sale.order.state', [('name', '=', new_state)])
         if states:
             export_sale_state.delay(session, record_id, priority=20)
     return True
-
-
-def find_prestashop_state(session, sale_state, backend_id):
-    state_list_model = 'sale.order.state.list'
-    state_list_ids = session.search(
-        state_list_model,
-        [('name', '=', sale_state)]
-    )
-    for state_list in session.browse(state_list_model, state_list_ids):
-        if state_list.prestashop_state_id.backend_id.id == backend_id:
-            return state_list.prestashop_state_id.prestashop_id
-    return None
 
 
 @job
@@ -150,9 +150,17 @@ def export_sale_state(session, record_id):
         sale_ids = [sale_ids]
     for sale in session.browse(inherit_model, sale_ids):
         backend_id = sale.backend_id.id
-        new_state = find_prestashop_state(session, sale.state, backend_id)
-        if new_state is None:
-            continue
+        new_state = ORDER_STATUS_MAPPING[sale.state]
+        state_ids = session.search(
+            'sale.order.state', [('name', '=', new_state)])
+        prestashop_state_ids = session.search(
+            'prestashop.sale.order.state', [('openerp_id', '=', state_ids[0])])
+        prestashop_state = session.browse(
+            'prestashop.sale.order.state', prestashop_state_ids[0])
         env = get_environment(session, inherit_model, backend_id)
         sale_exporter = env.get_connector_unit(SaleStateExport)
-        sale_exporter.run(sale.prestashop_id, new_state)
+        if prestashop_state:
+            sale_exporter.run(
+                sale.prestashop_id, prestashop_state.prestashop_id)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
