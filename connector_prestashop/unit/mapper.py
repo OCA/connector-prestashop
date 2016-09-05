dc9b28c4352c078ad37f4152eaa17e4e435b7904
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-09-05 14:36:40 +0200

    Remove dead code
    * translation export importer/mapper: unused, still based on the old Mapper
      API (deprecated in January 2014)

--- connector_prestashop/unit/mapper.py
+++ connector_prestashop/unit/mapper.py
@@ -16,24 +16,3 @@ class PrestashopExportMapper(ExportMapper):
         elif column._type == 'float':
             res = str(res)
         return res
-
-
-class TranslationPrestashopExportMapper(PrestashopExportMapper):
-
-    def convert(self, records_by_language, fields=None):
-        self.records_by_language = records_by_language
-        first_key = records_by_language.keys()[0]
-        self._convert(records_by_language[first_key], fields=fields)
-        self._data.update(self.convert_languages(self.translatable_fields))
-
-    def convert_languages(self, records_by_language, translatable_fields):
-        res = {}
-        for from_attr, to_attr in translatable_fields:
-            value = {'language': []}
-            for language_id, record in records_by_language.items():
-                value['language'].append({
-                    'attrs': {'id': str(language_id)},
-                    'value': record[from_attr] or ''
-                })
-            res[to_attr] = value
-        return res
