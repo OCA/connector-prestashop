3713cb6996604372b8e83a10afaf137e4ca3cd4c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-09-06 13:49:04 +0200

    Integrate with account_payment_mode

--- connector_prestashop/models/__init__.py
+++ connector_prestashop/models/__init__.py
@@ -4,11 +4,11 @@
 from . import binding
 
 from . import account_invoice
+from . import account_payment_mode
 from . import account_tax
 from . import account_tax_group
 from . import delivery_carrier
 from . import mail_message
-from . import payment_method
 from . import prestashop_backend
 from . import product_category
 from . import product_image
