248b79b7ab91164bd8806e3e1120a84b71a6030f
sergio.teruel <sergio.teruel@tecnativa.com>
2016-09-12 18:51:00 +0200

    [IMP][8.0] connector_prestashop: Recompute binding quantities from quant moves

--- connector_prestashop/models/stock_move/common.py
+++ connector_prestashop/models/stock_move/common.py
@@ -20,32 +20,26 @@ class StockLocation(models.Model):
         return prestashop_locations
 
 
-class StockMove(models.Model):
-    _inherit = 'stock.move'
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    @api.model
+    def create(self, vals):
+        location_obj = self.env['stock.location']
+        ps_locations = location_obj.get_prestashop_stock_locations()
+        quant = super(StockQuant, self).create(vals)
+        if quant.location_id in ps_locations:
+            quant.product_id.update_prestashop_qty()
+        return quant
 
     @api.multi
-    def update_prestashop_quantities(self):
-        for move in self:
-            move.product_id.update_prestashop_qty()
-
-    @api.multi
-    def _recompute(self):
-        locations = self.env['stock.location'].get_prestashop_stock_locations()
-        for stock_move in self:
-            if stock_move.location_dest_id.id in locations.ids or \
-                    stock_move.location_id.id in locations.ids:
-                stock_move.update_prestashop_quantities()
-
-    @api.multi
-    def action_cancel(self):
-        res = super(StockMove, self).action_cancel()
-        if res:
-            self._recompute()
-        return res
-
-    @api.multi
-    def action_done(self):
-        res = super(StockMove, self).action_done()
-        if res:
-            self._recompute()
+    def write(self, vals):
+        location_obj = self.env['stock.location']
+        ps_locations = location_obj.get_prestashop_stock_locations()
+        for quant in self:
+            location = quant.location_id
+            res = super(StockQuant, self).write(vals)
+            if location in ps_locations:
+                quant.invalidate_cache()
+                quant.product_id.update_prestashop_qty()
         return res
