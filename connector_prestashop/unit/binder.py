e54c1e24164f1b32abb87e5221f610f72ca1a339
Simone Orsi <simahawk@gmail.com>
2016-10-11 09:41:22 +0200

    [fix] remove binder override for missing fix (ref connector/pull/207)

--- connector_prestashop/unit/binder.py
+++ connector_prestashop/unit/binder.py
@@ -37,29 +37,3 @@ class PrestashopBinder(Binder):
         'prestashop.mail.message',
         'prestashop.groups.pricelist',
     ]
-
-    # method overrided here until https://github.com/OCA/connector/pull/207
-    # is merged
-    def to_openerp(self, external_id, unwrap=False):
-        """ Give the OpenERP ID for an external ID
-
-        :param external_id: external ID for which we want
-                            the OpenERP ID
-        :param unwrap: if True, returns the normal record
-                       else return the binding record
-        :return: a recordset, depending on the value of unwrap,
-                 or an empty recordset if the external_id is not mapped
-        :rtype: recordset
-        """
-        bindings = self.model.with_context(active_test=False).search(
-            [(self._external_field, '=', str(external_id)),
-             (self._backend_field, '=', self.backend_record.id)]
-        )
-        if not bindings:
-            if unwrap:
-                return getattr(self.model.browse(), self._openerp_field)
-            return self.model.browse()
-        bindings.ensure_one()
-        if unwrap:
-            bindings = getattr(bindings, self._openerp_field)
-        return bindings
