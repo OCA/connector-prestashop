8a05bf86727736900f3678169523847f978b8bad
Simone Orsi <simahawk@gmail.com>
2016-10-18 13:48:40 +0200

    [fix] retrieve `combination` key by version

--- connector_prestashop/models/product_template/importer.py
+++ connector_prestashop/models/product_template/importer.py
@@ -429,8 +429,8 @@ class ProductTemplateImporter(TranslatableRecordImporter):
         prestashop_record = self._get_prestashop_data()
         associations = prestashop_record.get('associations', {})
 
-        combinations = associations.get('combinations', {}).get(
-            'combinations', [])
+        ps_key = self.backend_record.get_version_ps_key('combination')
+        combinations = associations.get('combinations', {}).get(ps_key, [])
 
         if not isinstance(combinations, list):
             combinations = [combinations]
