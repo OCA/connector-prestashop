94143ec493722e828e594d90e4d89cbac9aecff8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-12-05 15:01:44 +0100

    fixup! [9.0][MIG] connector_prestashop_catalog_manager: Migration and new structure

--- connector_prestashop_catalog_manager/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import export_multiple_products
-from . import sync_products
-from . import active_deactive_products
-from . import export_category
