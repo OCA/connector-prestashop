b4729a4f43a8f80e5a8dbc3400782fe38bcc8a17
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-12-05 13:51:51 +0100

    Move conversion keys to a ConnectorUnit
    This way, we inherit from the 'backend inheritance'

---
+++ connector_prestashop/unit/version_key.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from openerp.addons.connector.connector import ConnectorUnit
+from ..backend import prestashop, prestashop_1_6_0_9, prestashop_1_6_1_12
+
+
+@prestashop
+class VersionKey(ConnectorUnit):
+    _model_name = '_prestashop.version.key'
+
+    keys = {}
+
+    def get_key(self, key):
+        return self.keys.get(key) or key
+
+
+@prestashop_1_6_0_9
+class VersionKey1609(VersionKey):
+
+    keys = {
+        'product_option_value': 'product_option_values',
+        'category': 'categories',
+        'order_slip': 'order_slips',
+        'order_slip_detail': 'order_slip_details',
+        'group': 'groups',
+        'order_row': 'order_rows',
+        'tax': 'taxes',
+        'image': 'images',
+        'combinations': 'combinations',
+        'tag': 'tags',
+    }
+
+
+@prestashop_1_6_1_12
+class VersionKey1612(VersionKey):
+
+    keys = {
+        'product_option_value': 'product_option_value',
+        'category': 'category',
+        'image': 'image',
+        'order_slip': 'order_slips',
+        'order_slip_detail': 'order_slip_details',
+        'group': 'group',
+        'order_row': 'order_rows',
+        'tax': 'taxes',
+        'combinations': 'combination',
+        'product_features': 'product_feature',
+        'tag': 'tag',
+    }
