91588b121df155b950233698fdfdd104661c1425
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-12-09 15:31:02 +0100

    Test import of inventory
    And modify the inventory importer to use the 'run' method of the
    Importer in order to benefit of the advisory lock and share the common
    flow.

--- connector_prestashop/tests/__init__.py
+++ connector_prestashop/tests/__init__.py
@@ -4,6 +4,7 @@ from . import test_auth
 from . import test_export_tracking
 from . import test_import_carrier
 from . import test_import_backend_data
+from . import test_import_inventory
 from . import test_import_partner
 from . import test_import_products
 from . import test_import_sale
