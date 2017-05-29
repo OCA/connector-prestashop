9f0cc3024836a00596b2916c434ed30477835d44
sergio.teruel <sergio.teruel@tecnativa.com>
2017-05-29 17:25:12 +0200

    [MIG] connector_prestashop: Begin of migration to v10

---
+++ connector_prestashop/backend.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import openerp.addons.connector.backend as backend
+
+
+prestashop = backend.Backend('prestashop')
+# version < 1.6.0.9
+prestashop_1_5_0_0 = backend.Backend(parent=prestashop, version='1.5')
+# version 1.6.0.9 - 1.6.0.10
+prestashop_1_6_0_9 = backend.Backend(parent=prestashop, version='1.6.0.9')
+# version >= 1.6.0.11
+prestashop_1_6_0_11 = backend.Backend(parent=prestashop, version='1.6.0.11')
+# version >= 1.6.1.2
+prestashop_1_6_1_2 = backend.Backend(parent=prestashop, version='1.6.1.2')
---
+++ connector_prestashop/models/product_image/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import common
+from . import deleter
+from . import importer
