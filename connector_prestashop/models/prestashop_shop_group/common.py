ce1234b0f75351f8c58c5884c2fe13e3772cd2e5
Benoit <benoit.guillot@akretion.com.br>
2018-04-10 23:02:49 +0200

    [FIX] connector_prestashop: More fixes
    * merge
    * component usage
    * product views
    * remove prestshop decorator and various fixes

--- connector_prestashop/models/prestashop_shop_group/common.py
+++ connector_prestashop/models/prestashop_shop_group/common.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
 from odoo.addons.component.core import Component
-from contextlib import contextmanager
-
 from odoo import models, fields, api, exceptions, _
-
-from odoo.addons.connector.connector import ConnectorEnvironment
-from ...components.importer import import_batch, import_record
-# from ...components.auto_matching_importer import AutoMatchingImporter
-from ...components.backend_adapter import  api_handle_errors
-from ...components.version_key import VersionKey
-
-
-from ..product_template.importer import import_inventory
-from ..product_supplierinfo.importer import import_suppliers
-from ..account_invoice.importer import import_refunds
-from ..sale_order.importer import import_orders_since
-
-
-_logger = logging.getLogger(__name__)
 
 
 class PrestashopShopGroup(models.Model):
@@ -46,6 +28,5 @@ class PrestashopShopGroup(models.Model):
 class ShopGroupAdapter(Component):
     _name = 'prestashop.shop.group.adapter'
     _inherit = 'prestashop.adapter'
-#     _model_name = 'prestashop.shop.group'
     _apply_on = 'prestashop.shop.group'
     _prestashop_model = 'shop_groups'
