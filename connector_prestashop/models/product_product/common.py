98c52dc2df700f2c86a242288cb385c70cc57931
Benoit <benoit.guillot@akretion.com.br>
2018-04-17 16:40:53 +0200

    [WIP] start to port tests

--- connector_prestashop/models/product_product/common.py
+++ connector_prestashop/models/product_product/common.py
@@ -210,12 +210,6 @@ class PrestashopProductCombination(models.Model):
             importer = work.component(usage='record.importer')
             return importer.set_variant_images(combination_ids, **kwargs)
 
-    @job(default_channel='root.prestashop')
-    def set_product_image_variant(self, backend, combination_ids, **kwargs):
-        with backend.work_on(self._name) as work:
-            importer = work.component(usage='record.importer')
-            return importer.set_variant_images(combination_ids, **kwargs)
-
 
 class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
