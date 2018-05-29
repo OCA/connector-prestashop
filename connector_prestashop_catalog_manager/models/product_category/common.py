77111c9226cc364e6520c7eb19dde0a013bf75bb
Benoit <benoit.guillot@akretion.com.br>
2018-05-29 14:37:53 +0200

    [FIX] listener

--- connector_prestashop_catalog_manager/models/product_category/common.py
+++ connector_prestashop_catalog_manager/models/product_category/common.py
@@ -7,7 +7,7 @@ from odoo.addons.component_event import skip_if
 
 class PrestashopProductCategoryListener(Component):
     _name = 'prestashop.product.category.event.listener'
-    _inherit = 'base.connector.listener'
+    _inherit = 'prestashop.connector.listener'
     _apply_on = 'prestashop.product.category'
 
     def _get_category_export_fields(self):
@@ -41,11 +41,11 @@ class PrestashopProductCategoryListener(Component):
 
 class ProductCategoryListener(Component):
     _name = 'product.category.event.listener'
-    _inherit = 'base.connector.listener'
+    _inherit = 'prestashop.connector.listener'
     _apply_on = 'product.category'
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
-    @skip_if(lambda self, record, **kwargs: self.need_to_export(record, **kwargs))
+    @skip_if(lambda self, record, **kwargs: self.need_to_export(record.prestashop_bind_ids, **kwargs))
     def on_record_write(self, record, fields=None):
         """ Called when a record is written """
         listener = self.component(
