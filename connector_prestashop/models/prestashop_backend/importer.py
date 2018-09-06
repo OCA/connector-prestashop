8e8718685845ff1fa26fd978595ec79de5383dce
@PlanetaTIC <@PlanetaTIC>
2018-09-06 15:04:28 +0200

    [FIX] connector_prestashop: Greenify branch
    * :green_apple: flake8
    * :green_apple: pylint
    * :green_apple: tests
    * :copyright: Add me as contributor

--- connector_prestashop/models/prestashop_backend/importer.py
+++ connector_prestashop/models/prestashop_backend/importer.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import _
 from odoo.addons.component.core import Component
-from odoo.addons.connector.components.mapper import mapping
 
 
 class MetadataBatchImporter(Component):
@@ -23,4 +21,3 @@ class MetadataBatchImporter(Component):
         'prestashop.shop.group',
         'prestashop.shop',
     ]
-
