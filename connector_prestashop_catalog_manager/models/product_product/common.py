93333c0968c36a19505b78fe3bb1ab211f1c9266
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2018-10-01 15:18:17 +0200

    [FIX] catalog_manager: Do not allow the modification of images from combinations to avoid exporting each of them several times due to a weird behavior of One2Many inverse and write listener

--- connector_prestashop_catalog_manager/models/product_product/common.py
+++ connector_prestashop_catalog_manager/models/product_product/common.py
@@ -5,6 +5,14 @@
 from openerp import models, fields
 
 
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    image_ids = fields.One2many(
+        inverse=False,
+    )
+
+
 class PrestashopProductCombination(models.Model):
     _inherit = 'prestashop.product.combination'
     minimal_quantity = fields.Integer(
