75df062707efc1e44bc4edc6c14182842da46b2c
@PlanetaTIC <@PlanetaTIC>
2018-11-30 13:42:49 +0100

    [IMP] connector_prestashop: New keys for version 1.6.1.12
    * This is a combination of 2 commits.
    
    [IMP] connector_prestashop: New keys for version 1.6.1.12
    [IMP] connector_prestashop: New keys for version 1.6.1.6

--- connector_prestashop/components/version_key.py
+++ connector_prestashop/components/version_key.py
@@ -70,3 +70,28 @@ class VersionKey1612(Component):
         "messages": "customer_messages",
         "manufacturers": "manufacturers",
     }
+
+
+class VersionKey1616(Component):
+    _name = "_prestashop.version.key.1.6.1.6"
+    _inherit = "_prestashop.version.key"
+    _usage = "prestashop.version.key.1.6.1.6"
+
+    # keys checked in 1.6.1.12:
+    # * tax
+
+    keys = {
+        "product_option_value": "product_option_value",
+        "category": "category",
+        "image": "image",
+        "order_slip": "order_slip",
+        "order_slip_detail": "order_slip_detail",
+        "group": "group",
+        "order_row": "order_row",
+        "tax": "tax",
+        "combinations": "combination",
+        "product_features": "product_feature",
+        "tag": "tag",
+        "messages": "customer_messages",
+        "manufacturers": "manufacturers",
+    }
