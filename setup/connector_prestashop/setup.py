b5f721be34dacd83af120adeda10d60834819852
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-03-29 16:42:43 +0100

    [FIX] connector_prestashop: Override external dependencies to match PyPi

--- setup/connector_prestashop/setup.py
+++ setup/connector_prestashop/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'vcr': 'vcrpy==1.10.5',
+            },
+        },
+    },
 )
