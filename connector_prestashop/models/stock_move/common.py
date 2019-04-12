21e676c413239396d5467fec6409264ca945664d
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2019-04-12 15:07:34 +0200

    [FIX] connector_prestashop: Fix wrong PS quantity after 2 receptions

--- connector_prestashop/models/stock_move/common.py
+++ connector_prestashop/models/stock_move/common.py
@@ -30,6 +30,7 @@ class StockQuant(models.Model):
         ps_locations = location_obj.get_prestashop_stock_locations()
         quant = super(StockQuant, self).create(vals)
         if quant.location_id in ps_locations:
+            quant.invalidate_cache()
             quant.product_id.update_prestashop_qty()
         return quant
 
