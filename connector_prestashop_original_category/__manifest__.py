bbd0d346fead326ea4167db280be5da87a0bd7d8
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2019-07-22 15:50:17 +0200

    [FIX] connector_prestashop_original_category: Fix manifest

--- connector_prestashop_original_category/__manifest__.py
+++ connector_prestashop_original_category/__manifest__.py
@@ -3,15 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Connector Prestashop Original Category',
-    'summary': 'Use in Odoo the same categories as prestashop',
+    'summary': 'Use in Odoo the same categories as Prestashop',
     'version': '10.0.1.0.0',
-    'author': 'PlanetaTIC',
-    'website': 'https://www.planetatic.com',
+    'development_status': 'Production/Stable',
+    'author': 'PlanetaTIC, Odoo Community Association (OCA)',
+    'maintainers': ['PlanetaTIC'],
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/connector-prestashop',
     'category': 'Connector',
     'depends': [
         'connector_prestashop',
     ],
     'demo': [],
     'data': [],
+    'application': False,
     'installable': True,
 }
