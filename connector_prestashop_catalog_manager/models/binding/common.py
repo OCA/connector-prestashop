f23ebb74279552a8f85899a3969bf92b1ac3d932
Marc Poch <mpoch@planetatic.com>
2021-04-21 11:57:04 +0200

    [FIX]connector_prestashop_catalog_manager: Avoid duplicate duplicate jobs, because exporting translation before binding is created.

---
+++ connector_prestashop_catalog_manager/models/binding/common.py
@@ -0,0 +1,16 @@
+# Copyright 2021 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+
+
+class PrestashopBinding(models.AbstractModel):
+    _inherit = 'prestashop.binding'
+
+    @api.model
+    def create(self, vals):
+        ctx = self.env.context.copy()
+        ctx['catalog_manager_ignore_translation'] = True
+        res = super(PrestashopBinding, self.with_context(ctx)).create(vals)
+
+        return res
